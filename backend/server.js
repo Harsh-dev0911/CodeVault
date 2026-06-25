0884c216047fdb02ee1ad24c9e7806ac279d757a
Harsh-dev0911 <dixitadarsh549@gmail.com>
2026-06-25 20:28:19 +0530

    Wrap custom DNS configuration to only run outside Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const dns = require("dns");
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+if (!process.env.VERCEL) {
+  const dns = require("dns");
+  dns.setServers(["1.1.1.1", "8.8.8.8"]);
+}
 
 const express = require("express");
 const mongoose = require("mongoose");
